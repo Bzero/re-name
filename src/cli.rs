3996c24193359ab95d4ff2bccccaf9ae09c8f649
Bzero <B_0@posteo.net>
2024-12-11 17:57:50 +0100

    Minimal re-name.

---
+++ src/cli.rs
@@ -0,0 +1,25 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(name = "re-name", author, version, about, max_term_width = 128)]
+#[clap(disable_help_flag = true, disable_version_flag = true)]
+pub struct Options {
+    #[arg(id = "SOURCE", required = true, help = "The regex pattern matching the source files.")]
+    pub source: String,
+
+    #[arg(id = "DESTINATION", required = true, help = "The pattern matching the destination files.")]
+    pub destination: String,
+
+    #[arg(short = 'p', long = "preview", help = "Show preview without renaming any files.")]
+    //        alias = "dry-run",
+    pub preview: bool,
+
+    #[arg(short = 'v', long = "verbose", help = "Display what is being done.")]
+    pub verbose: bool,
+
+    #[arg(short='h', long="help", action=clap::ArgAction::Help, help="Print help.", required=false)]
+    pub print_help: (),
+
+    #[arg(short='V', long="version", action=clap::ArgAction::Version, help="Print version.", required=false)]
+    pub print_version: (),
+}
---
+++ src/main.rs
@@ -0,0 +1,155 @@
+mod cli;
+
+use anyhow::{anyhow, Result};
+use clap::Parser;
+use itertools::Itertools;
+use regex::bytes::RegexBuilder;
+use walkdir::WalkDir;
+
+use std::collections::BTreeMap;
+use std::ffi::OsStr;
+use std::io;
+use std::io::Write;
+use std::os::unix::ffi::OsStrExt;
+use std::path::{Path, PathBuf};
+use std::process;
+
+/// Main.
+fn main() {
+    let success = inner_main();
+
+    let exit_code = match success {
+        Ok(n) => {
+            println!("Renamed {} files.", n);
+            0
+        }
+        Err(e) => {
+            eprintln!("Aboring.");
+            eprintln!("{}", e);
+            1
+        }
+    };
+
+    process::exit(exit_code);
+}
+
+/// Inner main. Performs the actual work and returns the number of renamed files or an error.
+fn inner_main() -> Result<u32> {
+    let options = cli::Options::parse();
+
+    let name_map = get_name_map(&options.source, &options.destination, &options)?;
+
+    if !name_map.values().all_unique() {
+        return Err(anyhow!("Multiple sources map to the same destination file."));
+    }
+
+    if !name_map.values().chain(name_map.keys()).all_unique() {
+        return Err(anyhow!("Some source map to another source file."));
+    }
+
+    if name_map.values().any(|b| std::fs::exists(b).unwrap_or(true)) {
+        return Err(anyhow!("One of the destination files already exist."));
+    }
+
+    let mut stdout = io::stdout().lock();
+    let mut n_renamed: u32 = 0;
+
+    if name_map.is_empty() {
+        writeln!(stdout, "No files match the source pattern.")?;
+        return Ok(0);
+    }
+
+    for (source, destination) in name_map.iter() {
+        if options.verbose || options.preview {
+            writeln!(
+                stdout,
+                "rename '{}' ⇒ '{}'",
+                source.to_string_lossy(),
+                destination.to_string_lossy()
+            )?;
+        }
+
+        if !options.preview {
+            //TODO: This is prone to time-of-check, time-of-use errors.
+            if !(std::fs::exists(destination).unwrap_or(true)) {
+                std::fs::rename(source, destination)?;
+                n_renamed += 1;
+            } else {
+                writeln!(
+                    stdout,
+                    "File {} already exists, will not overwrite.",
+                    destination.to_string_lossy()
+                )?;
+            }
+        } else {
+            n_renamed += 1;
+        }
+    }
+
+    return Ok(n_renamed);
+}
+
+/// Get the map of source to destination filenames.
+fn get_name_map(
+    source_pattern: &String,
+    destination_pattern: &String,
+    options: &cli::Options,
+) -> Result<BTreeMap<PathBuf, PathBuf>> {
+    let source_regex = build_regex(source_pattern, &options)?;
+
+    let mut walker = WalkDir::new(Path::new("./"));
+    //Limit the possible files to the current directory for now.
+    walker = walker.max_depth(1);
+
+    let mut name_map: BTreeMap<PathBuf, PathBuf> = BTreeMap::new();
+
+    for possible_entry in walker {
+        let entry = match possible_entry {
+            Ok(e) => e,
+            Err(err) => {
+                let path = err.path().unwrap_or(Path::new(""));
+                if let Some(inner) = err.io_error() {
+                    match inner.kind() {
+                        io::ErrorKind::PermissionDenied => {
+                            eprintln!("Missing permission to access {}.", path.display())
+                        }
+                        _ => {
+                            eprintln!("Unable to access {}.", path.display())
+                        }
+                    }
+                }
+                continue;
+            }
+        };
+
+        //skip root
+        if entry.depth() == 0 {
+            continue;
+        }
+
+        let current_path = strip_current_dir(entry.path());
+
+        if source_regex.is_match(current_path.as_os_str().as_bytes()) {
+            let source = current_path;
+            let replaced = source_regex.replace(source.as_os_str().as_bytes(), destination_pattern.as_bytes());
+            let destination = Path::new(OsStr::from_bytes(&replaced));
+
+            if source != destination {
+                name_map.insert(source.to_path_buf(), destination.to_path_buf());
+            }
+        }
+    }
+
+    return Ok(name_map);
+}
+
+/// Build Regex matching a full search string according to pattern and options.
+fn build_regex(pattern: &String, options: &cli::Options) -> Result<regex::bytes::Regex, regex::Error> {
+    let full_pattern = String::from("^") + pattern + "$";
+    return RegexBuilder::new(&full_pattern).build();
+}
+
+/// Remove the `./` prefix from a path.
+pub fn strip_current_dir(path: &Path) -> &Path {
+    return path.strip_prefix(".").unwrap_or(path);
+}
