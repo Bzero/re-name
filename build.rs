8cb391999211c188ebc4ef44f62b3a1f40dd47a3
Bzero <B_0@posteo.net>
2025-02-18 15:33:19 +0100

    Basic man page.

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
-use clap::{CommandFactory, ValueEnum};
+use clap::{Command, CommandFactory, ValueEnum};
 use clap_complete::{generate_to, Shell};
+use clap_mangen::Man;
 use std::env;
 use std::fs;
 use std::io::Error;
@@ -9,16 +10,33 @@ include!("src/cli.rs");
 
 fn main() -> Result<(), Error> {
     if env::var("PROFILE").unwrap() == "release" {
-        let out_dir = Path::new("completions");
-        fs::create_dir_all(&out_dir).unwrap();
-
         let mut cmd = Options::command();
         cmd.build();
 
-        for &shell in Shell::value_variants() {
-            generate_to(shell, &mut cmd, "re-name", &out_dir)?;
-        }
+        generate_completions(&mut cmd)?;
+        generate_man(cmd)?;
     }
 
+    Ok(())
+}
+
+fn generate_completions(cmd: &mut Command) -> Result<(), Error> {
+    let out_dir = Path::new("completions");
+    fs::create_dir_all(&out_dir).unwrap();
+
+    for &shell in Shell::value_variants() {
+        generate_to(shell, cmd, "re-name", &out_dir)?;
+    }
+    Ok(())
+}
+
+fn generate_man(cmd: Command) -> Result<(), Error> {
+    let out_dir = Path::new("man");
+    fs::create_dir_all(&out_dir).unwrap();
+
+    let man = Man::new(cmd);
+    let mut buffer: Vec<u8> = Default::default();
+    man.render(&mut buffer)?;
+    std::fs::write(out_dir.join("re-name.1"), buffer)?;
     Ok(())
 }
